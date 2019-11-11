6961d33f2a7c7cffcb31267335328705fad6724e
Sandeep Singh <singhsz@bc.edu>
2019-11-11 12:15:19 -0500

    9.15 Complete

--- Snacktacular/SpotDetailViewController.swift
+++ Snacktacular/SpotDetailViewController.swift
@@ -23,7 +23,7 @@ class SpotDetailViewController: UIViewController {
     @IBOutlet weak var cancelBarButton: UIBarButtonItem!
     
     var spot: Spot!
-    var reviews: [Review] = []
+    var reviews: Reviews!
     let regionDistance: CLLocationDistance = 750 // 750 meters or about a half mile
     var locationManager: CLLocationManager!
     var currentLocation: CLLocation!
@@ -35,7 +35,12 @@ class SpotDetailViewController: UIViewController {
         tap.cancelsTouchesInView = false
         self.view.addGestureRecognizer(tap)
         
+        
+        
         // mapView.delegate = self
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         
         if spot == nil {
             spot = Spot()
@@ -54,10 +59,19 @@ class SpotDetailViewController: UIViewController {
             
             
         }
+        reviews = Reviews()
  
         let region = MKCoordinateRegion(center: spot.coordinate, latitudinalMeters: regionDistance, longitudinalMeters: regionDistance)
         mapView.setRegion(region, animated: true)
         updateUserInterface()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        reviews.loadData(spot: spot) {
+            self.tableView.reloadData()
+        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -75,7 +89,7 @@ class SpotDetailViewController: UIViewController {
             let destination = segue.destination as! ReviewTableViewController
             destination.spot = spot
             let selectedIndexPath = tableView.indexPathForSelectedRow!
-            destination.review = reviews[selectedIndexPath.row]
+            destination.review = reviews.reviewArray[selectedIndexPath.row]
         default:
             print("*** ERROR Did not have a segue in SpotDetailViewController prepare(for segue:)")
             
@@ -232,5 +246,17 @@ extension SpotDetailViewController: CLLocationManagerDelegate {
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print("Failed to get user location.")
+    }
+}
+
+extension SpotDetailViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return reviews.reviewArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ReviewCell", for: indexPath) as! SpotReviewsTableViewCell
+        cell.review = reviews.reviewArray[indexPath.row]
+        return cell
     }
 }
